797134ee73384a1b43aa2b8171d822c2a08f9c92
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-08-28 19:46:21 +0530

    Updates

--- src/app/business/business.page.ts
+++ src/app/business/business.page.ts
@@ -1,15 +1,34 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NewsHeadlineService } from './../service/newsHeadline/news-headline.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-business',
-  templateUrl: './business.page.html',
-  styleUrls: ['./business.page.scss'],
+	selector: "app-business",
+	templateUrl: "./business.page.html",
+	styleUrls: ["./business.page.scss"],
 })
 export class BusinessPage implements OnInit {
+	News: any[] = [];
+	constructor(
+		private news: NewsHeadlineService,
+		private http: HttpClient,
+		private router: Router,
+	) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.http
+			.get<{ articles: any[] }>(
+				"http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=f7e6050983c14d0cb675dc8f8b6164f2",
+			)
+			.subscribe((re) => {
+				this.News = re.articles;
+				console.log(this.News);
+			});
+	}
+	readMore(news) {
+		this.news.setNewsReadMore(news);
+		console.log(news);
+		this.router.navigate(["/read-more"]);
+	}
 }
